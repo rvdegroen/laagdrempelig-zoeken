18c52cc22fa837b5ecdbdc129235f394dbeba135
AllyssaA <a.alimoestar@gmail.com>
2023-06-05 15:25:44 +0200

    :fire::recycle::art: refactor code

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,10 +1,49 @@
 // if you write something in line it shows up in http://localhost:4400/api/example
 // in frontend: fetch(/api/example)
-exports.getExample = function (req, res) {
-    res.json({ hello: 'idk' });
+// exports.getExample = function (req, res) {
+//     res.json({ hello: 'idk' });
+// };
+
+// // send http to api with fetch
+// exports.postExample = function (req, res) {
+//     res.json({});
+// };
+
+const fetch = require('node-fetch');
+
+const fetchData = async (req, res) => {
+  const publicKey = "0076bc3bc11d080e07a303360178002a";
+  const privateKey = "1a3b58ea286b7117a29af";
+
+  const apiUrl = "https://zoeken.oba.nl/api/v1/search/?q=harry";
+  const publicKeyParam = `&authorization=${publicKey}`;
+  const outputParam = "&output=json";
+
+  const apiUrlWithParams = apiUrl + publicKeyParam + outputParam;
+  const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+  const corsApiUrl = corsProxy + apiUrlWithParams;
+
+  try {
+    const response = await fetch(corsApiUrl, {
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const responseBody = await response.text();
+    console.log(responseBody); // Log the fetched data to the console
+    res.render("pages/index.ejs", { responseBody });
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "An error occurred" });
+  }
 };
 
-// send http to api with fetch
-exports.postExample = function (req, res) {
-    res.json({});
+module.exports = {
+  fetchData,
 };
+
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,20 @@
+// const express = require('express');
+// const router = express.Router();
+// const apiController = require('../controllers/apiController');
+
+// // in /api/example
+// router
+//     .route('/example/')
+//     .get(apiController.getExample)
+//     .post(apiController.postExample);
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
-const apiController = require('../controllers/apiController');
+const { fetchData } = require('../controllers/apiController');
 
-// in /api/example
-router
-    .route('/example/')
-    .get(apiController.getExample)
-    .post(apiController.postExample);
+router.get('/', fetchData);
 
 module.exports = router;
+
