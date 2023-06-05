632f15077fda2eb4c2cf9062ae450fa30291551f
AllyssaA <a.alimoestar@gmail.com>
2023-06-05 15:10:16 +0200

    :poop::construction: api fetch works in console

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // IMPORTS
 const express = require('express');
 const frontendRoutes = require('./routes/frontend');
+const fetch = require('node-fetch');
 // different routers
 const apiRoutes = require('./routes/api');
 
@@ -23,6 +24,36 @@ app.use('/api', apiRoutes);
 
 // api
 // app.use('/api', apiRouter);
+app.get("/fetch-data", async (req, res) => {
+    const publicKey = "0076bc3bc11d080e07a303360178002a";
+    // const privateKey = "1a3b58ea286b7117a29af";
+  
+    const apiUrl = "https://zoeken.oba.nl/api/v1/search/?q=harry";
+    const publicKeyParam = `&authorization=${publicKey}`;
+    const outputParam = "&output=json";
+  
+    const apiUrlWithParams = apiUrl + publicKeyParam + outputParam;
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+    const corsApiUrl = corsProxy + apiUrlWithParams;
+  
+    try {
+      const response = await fetch(corsApiUrl, {
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const responseBody = await response.text();
+      console.log(responseBody); // Log the fetched data to the console
+      res.render("index.ejs", { responseBody });
+    } catch (error) {
+      console.error("Error:", error.message);
+      res.status(500).json({ error: "An error occurred" });
+    }
+  });
+
 
 app.listen(port, () => {
     console.log(`The server is running on port http://localhost:${port}/`);
